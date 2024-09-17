db2b9627365beb17913df5e5b4fdaf563d53ed19
Martin <martin@apphancer.com>
2024-09-17 11:46:55 +0200

    Add deployment automation and bump PHP version to 8.3
    Added scripts and configurations for automated deployment via GitHub Actions, including a `deploy.sh` script. Created supporting Caddyfile, Dockerfile, and `docker-compose.prod.yml` for deployment. Also updated project to use PHP version 8.3 for improved compatibility and performance.

---
+++ deploy/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Exit the script as soon as a command fails.
+set -o errexit
+
+# Setup the variables
+VERSION=${VERSION:?Missing VERSION environment variable}
+CONTAINER_NAME=waldenburghofmarkt
+
+# Transform '.' into '-' in VERSION for tagging purposes
+VERSION=$(echo $VERSION | tr '.' '-')
+
+echo $VERSION
+
+# Clean up unnecessary files and copy environment file
+#rm -rf tests # do not delete here as this would cause issues with git checkout. Instead delete inside container
+
+
+sudo chown -R www-data /var/www/$CONTAINER_NAME
+sudo chgrp -R www-data /var/www/$CONTAINER_NAME
+
+# Start docker containers
+docker compose -p "${CONTAINER_NAME}-${VERSION}" -f deploy/docker-compose.prod.yml up --build --no-start
+docker compose -p "${CONTAINER_NAME}-${VERSION}" -f deploy/docker-compose.prod.yml up -d
+
+# Deployment tasks
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" composer install --no-scripts --no-dev --optimize-autoloader
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" composer dump-env prod
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" bin/console cache:clear --no-warmup --no-debug
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" bin/console cache:warmup
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" php bin/console importmap:install
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" php bin/console tailwind:build
+docker exec -u www-data "${CONTAINER_NAME}-${VERSION}-php-1" php bin/console asset-map:compile
+
+# Update Caddy
+cp deploy/Caddyfile /var/www/server/sites/$CONTAINER_NAME
+cd /var/www/server
+SITE_NAME=waldenburghofmarkt NEW_VERSION=$VERSION ./src/update-site.sh
+./src/build-config.sh
+docker compose restart
+
+# Remove old containers
+docker ps -a --format "{{.ID}}:{{.Names}}" |
+grep -E "${CONTAINER_NAME}-v[0-9]+-[0-9]+-[0-9]+-php-1" |
+grep -v "${CONTAINER_NAME}-${VERSION}-php-1" |
+awk -F":" '{print $1}' |
+xargs -n1 docker stop |
+xargs -n1 docker rm
